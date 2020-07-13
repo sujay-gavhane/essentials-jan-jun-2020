40b937f011099303a2478ab1da9f72262732cce4
Sujay Gavhane <sgavhane@xpanxion.com>
2020-07-13 12:13:06 +0530

    removed detail page link

--- memo/blog/urls.py
+++ memo/blog/urls.py
@@ -7,6 +7,5 @@ from .views import welcome, detail
 urlpatterns = [
     url(r'login$', LoginView.as_view(template_name="blog/login_form.html"), name='blogger_login'),
     url(r'logout$', LogoutView.as_view(), name="blogger_logout"),
-    url(r'^blog/<int:blog_id>$', detail, name='blog_detail'),
     url(r'^$', welcome, name="welcome")
 ]
